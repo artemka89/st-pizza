2ce977a19edc7eaf6c5bb3dbe863a1aab51f84df
Moshniaga Artem <artem.m89@mail.ru>
2024-07-02 20:18:02 +0300

    refactoring

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -1,12 +1,29 @@
 import { FC } from 'react';
 
+import { getPizzaImageUrl, PizzaOptions, useGetPizzas } from '@/entities/pizza';
 import { PageLayout } from '@/shared/ui/layouts/page-layout';
-import { PizzaList } from '@/widgets/pizza-list';
+import { ProductCardLayout } from '@/shared/ui/layouts/product-card-layout';
+import { ProductListLayout } from '@/shared/ui/layouts/product-list-layout';
 
 export const HomePage: FC = () => {
+  const pizzas = useGetPizzas();
+
+  if (pizzas.isPending) return '...Loading';
+
   return (
     <PageLayout>
-      <PizzaList />
+      <ProductListLayout>
+        {pizzas.data?.map((pizza) => (
+          <ProductCardLayout
+            key={pizza.id}
+            productItem={pizza}
+            actions={
+              <PizzaOptions options={pizza.options} addToCart={() => {}} />
+            }
+            imageUrl={() => getPizzaImageUrl({ id: pizza.imageId })}
+          />
+        ))}
+      </ProductListLayout>
     </PageLayout>
   );
 };

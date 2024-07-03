9fb391e547ebe4964f65624ac610f15c3f5c16d9
Moshniaga Artem <artem.m89@mail.ru>
2024-07-03 11:46:10 +0300

    fix style

--- src/shared/ui/layouts/product-list-layout.tsx
+++ src/shared/ui/layouts/product-list-layout.tsx
@@ -2,7 +2,7 @@ import { FC, PropsWithChildren } from 'react';
 
 export const ProductListLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className='grid w-full grid-cols-product-card justify-center'>
+    <div className='grid w-full grid-cols-product-card justify-center gap-4'>
       {children}
     </div>
   );
